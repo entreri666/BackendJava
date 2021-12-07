ed30ef0ca2e67e11d8b90d968442e82e13dfcbe0
entreri666 <entreri666@gmail.com>
2021-12-07 23:32:59 +0300

    HomeWork3-1

---
+++ src/test/java/AccountTests.java
@@ -0,0 +1,30 @@
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class AccountTests extends BaseTest {
+
+    @Test
+    void getAccountInfoWithAssertionsInGivenTest() {
+        given()
+                .header("Authorization", token)
+                .log()
+                .method()
+                .log()
+                .uri()
+                .expect()
+                .statusCode(200)
+                .contentType("application/json")
+                .body("data.url", equalTo(username))
+                .body("success", equalTo(true))
+                .body("status", equalTo(200))
+                .when()
+                .get("https://api.imgur.com/3/account/{username}", username)
+                .prettyPeek();
+
+    }
+}
